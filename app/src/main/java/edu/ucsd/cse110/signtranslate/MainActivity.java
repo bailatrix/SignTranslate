78273becc2e6a2fc021c7c0063a731f334f09516
Jigyasaa Sood <jsood@ucsd.edu>
2019-04-06 15:18:56 -0700

    very basic, take a photo camera functionality done

--- app/src/main/java/edu/ucsd/cse110/signtranslate/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/signtranslate/MainActivity.java
@@ -1,13 +1,40 @@
 package edu.ucsd.cse110.signtranslate;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        picture = (ImageView) findViewById(R.id.pictureView);
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        picture.setImageBitmap(bitmap);
     }
 }
